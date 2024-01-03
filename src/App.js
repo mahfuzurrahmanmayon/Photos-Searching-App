e489b23ae3fc1504ae663e061f413c52ae7478b7
Mahfuzur Rahman <mahfujurrahman291003@gmail.com>
2024-01-03 17:11:16 +0600

    complete this fun project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import axios from "axios"
 function App() {
+
+  const accessKey = "ukSmy95Un_nFHdBG3ueEJkPbTqEOAe4crdn4plVkC1A";
+  const [search, setSearch] = useState("")
+  const [photos, setPhotos] = useState([])
+  
+
+  const handleInput = (e)=> {
+    setSearch(()=>{
+      return e.target.value
+    })
+  }
+
+  const getImages = () => {
+    const urlApi = "https://api.unsplash.com/search/photos?page=1&query=" + search + "&client_id=" + accessKey;
+    axios.get(urlApi).then((response) => {
+      setPhotos(response.data.results)
+      console.log(response.data.results[0])
+    })
+    setSearch('')
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="photos-search-wrapper">
+      <h1>Photos Searching App</h1>
+      <div className="photo-input-wrapper">
+        <input type="text" placeholder="Photo Search..." value={search} onChange={handleInput} />
+        <button onClick={getImages}>Search</button>
+      </div>
+      <div className="search-image-wrapper">
+        {photos.map((photo) => (
+          <div key={photo.id} className="search-image-box">
+            <img src={photo.urls.small} alt={photo.alt_description } />
+            <p>{photo.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
